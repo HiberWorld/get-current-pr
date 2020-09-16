b38f5b2e25fdb733dfa8672e07c94953dd1700e7
Thehink <benja280@gmail.com>
2020-09-16 16:56:14 +0200

    Test

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,39 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {context, getOctokit} from '@actions/github'
+
+const getInputBool = (name: string, defaultValue = false): boolean => {
+  const param = core.getInput(name)
+  if (param === 'true' || param === '1') {
+    return true
+  } else if (param === 'false' || param === '0') {
+    return false
+  } else return defaultValue
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token = core.getInput('github-token', {required: true})
+    const sha = core.getInput('sha')
+    const filterOutClosed = getInputBool('filterOutClosed')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const result = await getOctokit(
+      token,
+      {}
+    ).repos.listPullRequestsAssociatedWithCommit({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      commit_sha: sha || context.sha
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    let pr = result.data.length > 0 && result.data[0]
+
+    if (filterOutClosed === true) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      pr = (pr as any).state === 'open' && pr
+    }
+
+    core.setOutput('number', (pr && pr.number) || '')
+    core.setOutput('pr', pr ? JSON.stringify(pr) : '')
   } catch (error) {
     core.setFailed(error.message)
   }
